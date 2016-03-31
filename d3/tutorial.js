6dade7dd4a64f30dad25058c7569cf493060668d
Jorge <jogonzal@microsoft.com>
2016-03-30 18:59:15 -0700

    Fix encoding

--- d3/tutorial.js
+++ d3/tutorial.js
@@ -11,8 +11,23 @@ var dataset = [
     [ 220,   88 ]
 ];
 
+var w = 10, h = 10;
+
 //Create SVG element
 var svg = d3.select("body")
     .append("svg")
     .attr("width", w)
     .attr("height", h);
+
+svg.selectAll("circle")
+    .data(dataset)
+    .enter()
+    .append("circle")
+
+.attr("cx", function(d) {
+    return d[0];
+})
+    .attr("cy", function(d) {
+        return d[1];
+    })
+    .attr("r", 5);
